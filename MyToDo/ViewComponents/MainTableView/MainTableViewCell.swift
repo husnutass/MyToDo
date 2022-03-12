344acf6ef357f73e8bdac245c261c529356d12b7
Hüsnü Taş <husnutas@outlook.com>
2022-03-12 16:43:55 +0300

    Add description label to cell

--- MyToDo/ViewComponents/MainTableView/MainTableViewCell.swift
+++ MyToDo/ViewComponents/MainTableView/MainTableViewCell.swift
@@ -9,18 +9,43 @@ import UIKit
 
 class MainTableViewCell: UITableViewCell {
     
-    lazy var titleLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textAlignment = .left
-        return label
-    }()
-    
     private var cellData: TodoItem?
     
     static var identifier: String {
         String(describing: self)
     }
+    
+    private lazy var mainStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [titleLabel, descriptionLabel])
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.spacing = 5
+        view.distribution = .fill
+        view.alignment = .fill
+        return view
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = .boldSystemFont(ofSize: 20)
+        label.numberOfLines = 1
+        label.lineBreakMode = .byTruncatingTail
+        label.setContentHuggingPriority(.defaultLow, for: .vertical)
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 16)
+        label.numberOfLines = 2
+        label.lineBreakMode = .byTruncatingTail
+        label.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        return label
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -33,10 +58,9 @@ class MainTableViewCell: UITableViewCell {
     
     func setupView() {
         addSubview(contentView)
-        contentView.addSubview(titleLabel)
+        contentView.addSubview(mainStackView)
         
-        titleLabel.expandView(to: contentView, with: 20)
-        
+        mainStackView.expandView(to: contentView, with: 20)
     }
     
     func setData(with data: TodoItem?) {
@@ -46,6 +70,7 @@ class MainTableViewCell: UITableViewCell {
     
     private func updateViewData() {
         titleLabel.text = cellData?.title
+        descriptionLabel.text = cellData?.todoListDescription
     }
     
 }
